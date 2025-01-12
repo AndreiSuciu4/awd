9eccff39bf5f51ef26cfc2fb9a61185735137ff7
Suciu Andrei <andreisuciu146@gmail.com>
2025-01-12 10:52:51 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import {NgIf, NgOptimizedImage} from '@angular/common';
+import {map} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    NgIf,
+    NgOptimizedImage
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'team';
+  sidebarOpened = false;
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([`(min-width: 850px)`])
+      .pipe(
+        map(result => result.matches)
+      )
+      .subscribe(isLargeScreen => {
+        if (isLargeScreen) {
+          this.sidebarOpened = false;
+        }
+      });
+  }
+  toggleSidebar() {
+    this.sidebarOpened = !this.sidebarOpened;
+  }
 }
